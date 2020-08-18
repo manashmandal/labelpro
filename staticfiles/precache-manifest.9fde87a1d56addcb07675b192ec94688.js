3697da5ef8e0489fd61d097db6888eedff0f768b
Manash Kumar Mandal <manashmndl@gmail.com>
2020-08-18 09:16:24 +0600

    react django integration

---
+++ staticfiles/precache-manifest.9fde87a1d56addcb07675b192ec94688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ea72d4af2691ce068113e06a116984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4ea8511d64d39ac18c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37b51ec9382a17cddc48",
+    "url": "/static/js/2.75f9f306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75f9f306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ea8511d64d39ac18c5",
+    "url": "/static/js/main.62f19413.chunk.js"
+  },
+  {
+    "revision": "a8d4984627f5fa5ab9b7",
+    "url": "/static/js/runtime-main.4210616f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
